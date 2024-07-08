6edf9954d502eebb712ee0106d3fac33296403cf
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2024-07-08 18:25:07 +0300

    Update pci.h

--- src/drivers/pci.h
+++ src/drivers/pci.h
@@ -3,8 +3,27 @@
 
 #include <stdint.h>
 
+#define PCI_CONFIG_ADDRESS 0xCF8
+#define PCI_CONFIG_DATA 0xCFC
+
+typedef struct {
+    uint16_t vendor_id;
+    uint16_t device_id;
+    uint16_t command;
+    uint16_t status;
+    uint8_t revision_id;
+    uint8_t prog_if;
+    uint8_t subclass;
+    uint8_t class_code;
+    uint8_t cache_line_size;
+    uint8_t latency_timer;
+    uint8_t header_type;
+    uint8_t bist;
+} pci_device_header_t;
+
 void pci_init(void);
-uint32_t pci_read(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
-void pci_write(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset, uint32_t data);
+uint32_t pci_read_config_dword(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
+void pci_write_config_dword(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset, uint32_t value);
+void pci_enumerate_devices(void);
 
 #endif // PCI_H
