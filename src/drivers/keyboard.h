10028afa0e648d58422cea2d9ca7b1831b959fc2
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2024-07-08 18:32:36 +0300

    Update keyboard.h

--- src/drivers/keyboard.h
+++ src/drivers/keyboard.h
@@ -2,13 +2,24 @@
 #define KEYBOARD_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
-#define KEYBOARD_DATA_PORT 0x60
-#define KEYBOARD_STATUS_PORT 0x64
-#define KEYBOARD_COMMAND_PORT 0x64
+// Скан-коды клавиш
+#define KEY_ESCAPE  0x01
+#define KEY_ENTER   0x1C
+#define KEY_LSHIFT  0x2A
+#define KEY_RSHIFT  0x36
+#define KEY_LCTRL   0x1D
+#define KEY_LALT    0x38
+
+typedef void (*keyboard_callback_t)(uint8_t scancode);
 
 void keyboard_init(void);
-uint8_t keyboard_read_scan_code(void);
-char keyboard_scan_code_to_ascii(uint8_t scan_code);
+uint8_t keyboard_read(void);
+char keyboard_scancode_to_ascii(uint8_t scancode);
+bool keyboard_is_key_pressed(uint8_t scancode);
+void keyboard_set_callback(keyboard_callback_t callback);
+void keyboard_enable_interrupts(void);
+void keyboard_disable_interrupts(void);
 
-#endif // KEYBOARD_H
+#endif
