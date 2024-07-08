5e0b1c92270db11f628aab2d18e338211006ba25
MikhailSzhilko <144871094+Asphorr@users.noreply.github.com>
2024-07-08 18:32:06 +0300

    Update vga.h

--- src/drivers/vga.h
+++ src/drivers/vga.h
@@ -2,32 +2,52 @@
 #define VGA_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
-#define VGA_WIDTH 80
-#define VGA_HEIGHT 25
+// Стандартные режимы VGA
+#define VGA_MODE_TEXT_80x25   0x03
+#define VGA_MODE_GRAPHICS_320x200 0x13
 
-#define VGA_COLOR_BLACK 0
-#define VGA_COLOR_BLUE 1
-#define VGA_COLOR_GREEN 2
-#define VGA_COLOR_CYAN 3
-#define VGA_COLOR_RED 4
-#define VGA_COLOR_MAGENTA 5
-#define VGA_COLOR_BROWN 6
-#define VGA_COLOR_LIGHT_GREY 7
-#define VGA_COLOR_DARK_GREY 8
-#define VGA_COLOR_LIGHT_BLUE 9
-#define VGA_COLOR_LIGHT_GREEN 10
-#define VGA_COLOR_LIGHT_CYAN 11
-#define VGA_COLOR_LIGHT_RED 12
-#define VGA_COLOR_LIGHT_MAGENTA 13
-#define VGA_COLOR_LIGHT_BROWN 14
-#define VGA_COLOR_WHITE 15
+// Цвета
+typedef enum {
+    VGA_COLOR_BLACK,
+    VGA_COLOR_BLUE,
+    VGA_COLOR_GREEN,
+    VGA_COLOR_CYAN,
+    VGA_COLOR_RED,
+    VGA_COLOR_MAGENTA,
+    VGA_COLOR_BROWN,
+    VGA_COLOR_LIGHT_GREY,
+    VGA_COLOR_DARK_GREY,
+    VGA_COLOR_LIGHT_BLUE,
+    VGA_COLOR_LIGHT_GREEN,
+    VGA_COLOR_LIGHT_CYAN,
+    VGA_COLOR_LIGHT_RED,
+    VGA_COLOR_LIGHT_MAGENTA,
+    VGA_COLOR_LIGHT_BROWN,
+    VGA_COLOR_WHITE
+} vga_color_t;
 
-void vga_init(void);
-void vga_putchar(char c, uint8_t color, uint8_t x, uint8_t y);
-void vga_write(const char* str, uint8_t color);
-void vga_write_at(const char* str, uint8_t color, uint8_t x, uint8_t y);
+// Инициализация VGA
+void vga_init(uint8_t mode);
+
+// Функции для текстового режима
+void vga_putchar(char c);
+void vga_write(const char* str);
 void vga_clear(void);
-void vga_set_cursor(int x, int y);
+void vga_set_color(vga_color_t fg, vga_color_t bg);
+void vga_set_cursor(uint8_t x, uint8_t y);
 
-#endif // VGA_H
+// Функции для графического режима
+void vga_set_pixel(uint16_t x, uint16_t y, uint8_t color);
+void vga_draw_line(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint8_t color);
+void vga_draw_rect(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint8_t color, bool fill);
+
+// Переключение режимов
+void vga_set_mode(uint8_t mode);
+
+// Получение информации о текущем режиме
+uint8_t vga_get_mode(void);
+void vga_get_resolution(uint16_t* width, uint16_t* height);
+
+#endif
